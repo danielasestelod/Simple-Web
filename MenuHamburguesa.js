00ec8472fbb4eddbb48c3de7ac63016904808483
DanielaSD <danielasestelo42@gmail.com>
2024-01-22 14:33:12 +0100

    primer commit

--- MenuHamburguesa.js
+++ MenuHamburguesa.js
@@ -5,7 +5,7 @@ class MenuHamburguesa{
             document.getElementById("opciones_menu_hamburguesa").classList.remove("hidden");
         });
     }
-    //puedes usar toggle o add igual que en la función de arriba
+    
     listenerCerrar(){
         document.getElementById("icono_x").addEventListener("click",()=>{
             document.getElementById("opciones_menu_hamburguesa").classList.toggle("hidden");
